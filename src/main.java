941fdf38ba96ef79478a7ecb44e5a40ed7c3dee3
Student <Student@DESKTOP-KCT2FLC>
2018-03-27 16:17:09 +0530

    After changes  renaming,extract method,move

--- src/main.java
+++ src/main.java
@@ -8,7 +8,7 @@ public class main {
 		Movie m1=new Movie("Doom",2);
 	
 		Customer c=new Customer("ashitosh");
-		c.addRental(new Rental(m,4));
+		c.addRental(new Rental(m,10));
 		c.addRental(new Rental(m1, 5));
 		System.out.println(c.statement());
 	}
